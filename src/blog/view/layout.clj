7475f1e82b52d86422e539bbe922c120217263b4
bouro412 <keita.bouro@gmail.com>
2017-08-22 18:55:24 +0900

    いつでもログイン、ログアウトができるようレイアウトを変更

--- src/blog/view/layout.clj
+++ src/blog/view/layout.clj
@@ -1,5 +1,6 @@
 (ns blog.view.layout
-  (:require [hiccup.page :refer [html5 include-css include-js]]))
+  (:require [hiccup.page :refer [html5 include-css include-js]]
+            [blog.util.login :refer [login-user login?]]))
 
 (defn common [req & body]
   (html5
@@ -10,5 +11,13 @@
                  "/css/style.css")
     (include-js "/js/main.js")]
    [:body
-    [:header.top-bar.bg-green.depth-3 "blog"]
+    (if-let [user (login-user req)]
+      [:header.top-bar.bg-green.depth-3
+       "blog "
+       (:name user)
+       " "
+       [:a {:href "/logout"} "ログアウト"]]
+      [:header.top-bar.bg-green.depth-3
+       "blog "
+       [:a {:href "/login"} "ログイン"]])
     [:main body]]))
