a111fdbe142d6b453cfcc412a88f22e10fb3c7f7
bouro412 <keita.bouro@gmail.com>
2017-08-22 20:07:06 +0900

    routingの書き方を修正

--- src/blog/handler/user.clj
+++ src/blog/handler/user.clj
@@ -1,6 +1,7 @@
 (ns blog.handler.user
   (:require [compojure.core :refer [defroutes context GET POST]]
             [compojure.route :as route]
+            [compojure.coercions :refer [as-int]]
             [blog.view.user :as view]
             [blog.handler.util :refer [html]]
             [blog.db.user :refer [get-users id->uid]]
@@ -8,17 +9,17 @@
             [blog.util.login :refer [login-user]]
             [ring.util.response :as res]))
 
-(defn user-home [{:as req :keys [params]}]
-  (if-let [author (first (get-users :user_id (:user-id params)))]
+(defn user-home [{:as req :keys [params]} user-id]
+  (if-let [author (first (get-users :user_id user-id))]
     (-> (view/user-home-view req author)
         (res/response)
         html)
     (res/not-found (format "user %s is not found."
                            (:user-id params)))))
 
-(defn article-show [{:as req :keys [params]}]
-  (if-let [article (first (get-articles :owner_id (id->uid (:user-id params))
-                                        :uid (Integer/parseInt(:article-id params))))]
+(defn article-show [{:as req :keys [params]} user-id article-uid]
+  (if-let [article (first (get-articles :owner_id (id->uid user-id)
+                                        :uid article-uid))]
     (-> (view/article-view req article (:user-id params))
         (res/response)
         html)
@@ -36,10 +37,11 @@
       (res/not-found "article is not found"))))
 
 (defroutes user-routes
-  (context "/user/:user-id" _
-           (GET "/" _ user-home)
-           (GET "/:article-id" _ article-show)
-           ))
+  (context "/user/:user-id" [user-id]
+           (GET "/" [:as req] (user-home req user-id))
+           (GET ["/:article-id" :article-id #"[0-9]+"]
+                [article-id :<< as-int :as req]
+                (article-show req user-id article-id))))
 (defroutes edit-routes
 ;  (GET "/edit/:user-id" _ edit-new)
   (GET "/edit/:user-id/:article-id" _ edit))
